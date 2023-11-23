037b48fb7e5e9eb0a802a263fb6dfe30dd4da062
aletardio <ale.tardio99@gmail.com>
2023-11-23 17:15:45 +0100

    Stamp messagge script on html

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,15 +1,27 @@
-let player = Math.floor(Math.random()*7);
-let cpu = Math.floor(Math.random()*7);
+// CREAZIONE DI VARIABILI PER LA GENERAZIONE RANDOMICA DI NUMERI DA 1 A 6 
+let playerScore = Math.floor(Math.random()*7);
+let cpuScore = Math.floor(Math.random()*7);
 
-console.log('player = ' + player);
-console.log('cpu = ' + cpu);
+console.log('Giocatore = ' + playerScore);
+console.log('CPU = ' + cpuScore);
 
-if(player > cpu) {
-    console.log('player vince');
+// RISULTATO STAMPATO IN HTML
+let result = document.getElementById('result');
+
+// CICLO IF PER STABILIRE IL VINCITORE IN BASE A CHI FA IL PUNTEGGIO PIU' ALTO
+if(playerScore > cpuScore) {
+    // VITTORIA GIOCATORE
+    result.innerHTML = 'Giocatore Vince!';
+    console.log('Giocatore vince');
 } 
-else if(player < cpu) {
-    console.log('cpu vince');
+else if(playerScore < cpuScore) {
+    // VITTORIA COMPUTER
+    result.innerHTML = 'CPU Vince!';
+    console.log('CPU vince');
 } 
 else {
-    console.log('pareggio');
+    // PAREGGIO PER ENTRAMBI
+    result.innerHTML = 'Pareggio!';
+    console.log('Pareggio');
 }
+
